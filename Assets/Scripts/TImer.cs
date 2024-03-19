858c49f07fb5b57afd2130389f308a925f98ae7e
Gameboy16w <87306273+Gameboy16w@users.noreply.github.com>
2024-03-19 13:45:27 +0100

    end screen

--- Assets/Scripts/TImer.cs
+++ Assets/Scripts/TImer.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using System;
+using UnityEngine.SceneManagement;
+
 public class TImer : MonoBehaviour
 {
     public float timer = 60;
     public TMP_Text Text;
+
+    public GameObject EndScreen;
 
     // Start is called before the first frame update
 
@@ -18,5 +22,15 @@ public class TImer : MonoBehaviour
         Text.SetText(timer.ToString("F0"));
        timer -= Time.deltaTime;
        Console.WriteLine(timer);
+       if (timer <= 0)
+       {
+            EndScreen.SetActive(true);
+       }
+    }
+        public void ResetScene()
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+
+        SceneManager.LoadScene(currentSceneName);
     }
 }
