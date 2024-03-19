8aee28f669a6f87214ebc47bf24b103ad2b6a1ad
Joas Sahetapy <102023319+JSTappy@users.noreply.github.com>
2024-03-19 13:36:04 +0100

    Adding Score Display & Credits

--- Assets/Scripts/RecycleBin.cs
+++ Assets/Scripts/RecycleBin.cs
@@ -1,39 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class RecycleBin : MonoBehaviour
 {
     private int _score = 0;
     public List<Bottle> bottles;
+    private TextMeshProUGUI _textMeshPro;
     // Start is called before the first frame update
     void Start()
     {
+        _textMeshPro = GameObject.Find("Score").GetComponent<TextMeshProUGUI>();
         bottles = new List<Bottle>();    
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            //make a new bottle and add it to the list
-            Bottle newBottle = new Bottle();
-            bottles.Add(newBottle);
-            UpdateScore();
-        }
-        
+
     }
 
     public void UpdateScore()
     {
         _score = bottles.Count;
-        Debug.Log("Score: " + _score);
+        _textMeshPro.text = "Score : " + _score;
     }
 
     public void AddBottle(Bottle bottle)
     {
         bottles.Add(bottle);
+        UpdateScore();
     }
 
 }
